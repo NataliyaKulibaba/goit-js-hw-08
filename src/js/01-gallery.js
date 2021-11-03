40a9935802770b805b9dbb8b83e7d0db69972a59
NataliyaKulibaba <natakulibaba88@gmail.com>
2021-11-03 23:53:12 +0200

    1 task done

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import "simplelightbox/dist/simple-lightbox.min.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// import SimpleLightbox from "simplelightbox";
+
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+const imagesContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}" >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}" 
+    />
+  </a> `;
+  }).join('');
+};
+
+
+  const lightbox = new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay: 250 });
+
